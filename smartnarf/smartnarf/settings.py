fd1a3a18c5d028e033cd4264fcf6d7bbd24cb3bc
EGR <egor-good2000@mail.ru>
2022-03-04 01:32:34 +0300

    Add static settings and folder

--- smartnarf/smartnarf/settings.py
+++ smartnarf/smartnarf/settings.py
@@ -109,12 +109,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    STATIC_URL,
 ]
 
-STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
